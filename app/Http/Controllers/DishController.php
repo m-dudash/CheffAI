dbcc86586fc89ef10131cdabefe8973d8ad886a3
Mykhailo Dudash <dudasmika17@gmail.com>
2025-09-02 16:58:38 +0200

    refactor: connect controller and service (dish)

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DishRequest;
+use App\Services\DishService;
 use Illuminate\Http\Request;
 
 class DishController extends Controller
 {
-    public function __construct(private DishController $dishController) { }
+    public function __construct(private DishService $dishService) { } //dependency injection
 
     public function generate(DishRequest $request)
     {
-
+        $dto = $this->dishService->generateDish($request->dish);
+        return response()->json($dto->toArray());
     }
 }
